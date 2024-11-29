e4eea52a9b56fd54a1d5da551c35b15dd04a0376
Akash <117592488+akashbhatc@users.noreply.github.com>
2024-11-29 18:10:56 +0530

    Update app.js

--- safeSpace/app.js
+++ safeSpace/app.js
@@ -3,7 +3,6 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
-
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
